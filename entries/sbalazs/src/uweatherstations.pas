96549b3edfde3f1f670065f44521294cbfd5d1aa
Gustavo Carreno <guscarreno@gmail.com>
2024-03-13 18:55:34 +0000

    chore: Forth submission from sbalazs

--- entries/sbalazs/src/uweatherstations.pas
+++ entries/sbalazs/src/uweatherstations.pas
@@ -5,20 +5,30 @@ unit uWeatherStations;
 interface
 
 uses
-  Classes, SysUtils, Math, syncobjs, contnrs;
+  Classes, SysUtils, Math, syncobjs;
 
 type
   TWSManager = class;
 
-  PWS = ^TWS;
-  TWS = record
-    FName: String[100];
+const
+  HashBuckets = 1 shl 17;
+
+type
+  PData = ^TData;
+  TData = record
     FMin: Single;
     FMax: Single;
-    FMean: Single;
     FTot: Single;
     FCnt: Integer;
+    FHash: QWord;
   end;
+
+  TWS = record
+    FStation: TBytes;
+    FData: PData;
+  end;
+
+  TWSList = array[0..HashBuckets - 1] of TWS;
 
   TLock = class
   private
@@ -46,8 +56,9 @@ type
     FStarted: Boolean;
     FWSManager: TWSManager;
     procedure Wait(AMs: Integer);
-    function GetLeftOver(AST: TStream): String;
+    procedure AddLineBreak(var ABytes: TBytes);
     function GetThreadCnt: Integer;
+    function GetNextLineBreak(AST: TStream): TBytes;
   protected
     procedure TerminatedSet; override;
   public
@@ -57,17 +68,17 @@ type
 
   TWSThread = class(TWSThreadBase)
   private
-    FStr: String;
-    FHashList: TFPHashList;
+    FBytes: TBytes;
     FMS: TMemoryStream;
-    procedure ProcessBuffer(ABuffer: String);
+    FWSList: TWSList;
     procedure UpdateMainHashList;
-    procedure AddToHashList(AStation, ATemp: String); inline;
+    procedure ProcessBytes(ABytes: TBytes);
+    procedure AddToHashList(AStation: TBytes; ATemp: Single; AHash: QWord);
     procedure UpdateThreadList(AUpdateType: TUpdateType);
   protected
     procedure Execute; override;
   public
-    constructor Create(AStr: String; AWSManager: TWSManager);
+    constructor Create(ABytes: TBytes; AWSManager: TWSManager);
     destructor Destroy; override;
   end;
 
@@ -80,15 +91,13 @@ type
     constructor Create(AWSManager: TWSManager);
   end;
 
-  { TWSManager }
-
   TWSManager = class
   private
     FSrcFile: String;
     FThreadCnt: Integer;
     FTerminated: Boolean;
     FThreadList: TLockList;
-    FHashListAll: TFPHashList;
+    FWSListALL: TWSList;
     FWSThreadsWatcher: TWSThreadsWatcher;
     FOnStart: TNotifyEvent;
     FOnFinish: TNotifyEvent;
@@ -150,26 +159,15 @@ begin
   FEvent.WaitFor(AMs);
 end;
 
-function TWSThreadBase.GetLeftOver(AST: TStream): String;
+procedure TWSThreadBase.AddLineBreak(var ABytes: TBytes);
 var
-  P, Len: Integer;
-  ReadCnt: LongInt;
+  Len: LongInt;
 begin
-  Result := '';
-  if AST.Position = AST.Size then
-    Exit;
-  Len := 100 div SizeOf(Char);
-  SetLength(Result, Len);
-  ReadCnt := AST.Read(Pointer(Result)^, Len);
-  if ReadCnt > 0 then
+  Len := Length(ABytes);
+  if ABytes[Len - 1] <> 10 then
   begin
-    AST.Position := AST.Position - ReadCnt;
-    P := Pos(sLineBreak, Result);
-    if P > 0 then
-    begin
-      Result := Copy(Result, 1, P - 1);
-      AST.Position := AST.Position + Length(Result)*SizeOf(Char);
-    end;
+    SetLength(ABytes, Len + 1);
+    ABytes[Len] := 10;
   end;
 end;
 
@@ -181,6 +179,40 @@ begin
     Result := FWSManager.FThreadList.FList.Count;
   finally
     FWSManager.FThreadList.Unlock;
+  end;
+end;
+
+function TWSThreadBase.GetNextLineBreak(AST: TStream): TBytes;
+var
+  ReadCnt: LongInt;
+  P, I: Integer;
+begin
+  Result := nil;
+  if AST.Position = AST.Size then
+    Exit;
+  SetLength(Result, 100);
+  ReadCnt := AST.Read(Result[0], 100);
+  if ReadCnt > 0 then
+  begin
+    P := 0;
+    AST.Position := AST.Position - ReadCnt;
+    for I := Low(Result) to High(Result) do
+    begin
+      if (Result[I] = 10) or (Result[I] = 0) then
+      begin
+        P := I;
+        Break;
+      end;
+    end;
+    if P > 0 then
+    begin
+      SetLength(Result, P + 1);
+      AST.Position := AST.Position + P + 1;
+      if AST.Position > AST.Size then
+         AST.Position := AST.Size;
+    end
+    else
+      Result := nil;
   end;
 end;
 
@@ -205,70 +237,37 @@ begin
 end;
 
 { TWSThread }
-constructor TWSThread.Create(AStr: String; AWSManager: TWSManager);
+constructor TWSThread.Create(ABytes: TBytes; AWSManager: TWSManager);
+var
+  I: Integer;
 begin
   inherited Create(AWSManager);
-  FStr := AStr;
-  FMS := TMemoryStream.Create;
-  FMS.Position := 0;
+  SetLength(FBytes, Length(ABytes));
+  Move(ABytes[0], FBytes[0], Length(ABytes));
   FStarted := False;
-  FHashList := TFPHashList.Create;
+  FMS := TMemoryStream.Create;
+  for I := Low(FWSList) to High(FWSList) do
+  begin
+    FWSList[I].FData := nil;
+    New(FWSList[I].FData);
+    FWSList[I].FData^.FMin := 0;
+    FWSList[I].FData^.FMax := 0;
+    FWSList[I].FData^.FTot := 0;
+    FWSList[I].FData^.FCnt := 0;
+  end;
 end;
 
 destructor TWSThread.Destroy;
 var
   I: Integer;
-  WS: PWS;
 begin
-  for I := 0 to FHashList.Count - 1 do
-  begin
-    WS := FHashList.Items[I];
-    Dispose(WS);
-  end;
-  FHashList.Clear;
-  FHashList.Free;
   FMS.Free;
+  for I := Low(FWSList) to High(FWSList) do
+    Dispose(FWSList[I].FData);
   inherited Destroy;
 end;
 
-procedure TWSThread.UpdateMainHashList;
-var
-  I: Integer;
-  WS: PWS;
-  WSAll: PWS;
-  Hash: ShortString;
-  Index: Integer;
-begin
-  for I := 0 to FHashList.Count - 1 do
-  begin
-    if FWSManager.FTerminated then
-      Break;
-    Hash := FHashList.NameOfIndex(I);
-    WS := FHashList.Items[I];
-    Index := FWSManager.FHashListAll.FindIndexOf(Hash);
-    if Index = -1 then
-    begin
-       New(WSAll);
-       WSAll^.FName := WS^.FName;
-       WSAll^.FMin := WS^.FMin;
-       WSAll^.FMax := WS^.FMax;
-       WSAll^.FTot := WS^.FTot;
-       WSAll^.FCnt := WS^.FCnt;
-       FWSManager.FHashListAll.Add(Hash, WSAll);
-    end
-    else
-    begin
-      WSAll := FWSManager.FHashListAll.Items[Index];
-      WSAll^.FMin := Min(WSALL^.FMin, WS^.FMin);
-      WSAll^.FMax := Max(WSAll^.FMax, WS^.FMax);
-      WSAll^.FTot := WSAll^.FTot + WS^.FTot;
-      WSAll^.FCnt := WSAll^.FCnt + WS^.FCnt;
-    end;
-  end;
-end;
-
 procedure TWSThread.UpdateThreadList(AUpdateType: TUpdateType);
-
 begin
   while (not Terminated) do
   begin
@@ -293,94 +292,183 @@ begin
   end;
 end;
 
-procedure TWSThread.AddToHashList(AStation, ATemp: String);
+procedure TWSThread.ProcessBytes(ABytes: TBytes);
 var
-  WS: PWS;
-  Index: Integer;
+  PCur: Int64;
+  PBeg: Int64;
+  PDel: Int64;
+  Len: Int64;
+  Station: TBytes;
   Temp: Single;
- Code: Integer;
+  DoHash: Boolean;
+  DoTemp: Boolean;
+  IsNeg: Boolean;
+  Hash: QWord;
 begin
-  Index := FHashList.FindIndexOf(AStation);
-  if Index = -1 then
-  begin
-    Val(ATemp, Temp, Code);
-    if Code <> 0 then Exit;
-
-    New(WS);
-    WS^.FName := AStation;
-    WS^.FMin := Temp;
-    WS^.FMax := Temp;
-    WS^.FTot := Temp;
-    WS^.FCnt := 1;
-    FHashList.Add(AStation, WS);
-  end
-  else
-  begin
-    Val(ATemp, Temp, Code);
-    if Code <> 0 then Exit;
-
-    WS := FHashList.Items[Index];
-    WS^.FMin := Min(WS^.FMin, Temp);
-    WS^.FMax := Max(WS^.FMax, Temp);
-    WS^.FTot := WS^.FTot + Temp;
-    WS^.FCnt := WS^.FCnt + 1;
-  end;
-end;
-
-procedure TWSThread.ProcessBuffer(ABuffer: String);
-var
-  P: Integer;
-  PDel, PEnd: Integer;
-  Str: String;
-  Station: String;
-  Temp: String;
-begin
-  Str := StringReplace(ABuffer, sLineBreak, ':', [rfReplaceAll]);
-  if Str[1] = ':' then
-    Delete(Str, 1, 1);
-  P := 1;
+  PCur := -1;
+  PBeg := 0;
+  PDel := 0;
+  Temp := 0;
+  Station := nil;
+  DoHash := True;
+  DoTemp := False;
+  Len := Length(ABytes);
+  Hash := 14695981039346656037; //FNV (Fowler-Noll-Vo)
   repeat
-    PDel := Pos(';', Str, P);
-    PEnd := Pos(':', Str, P);
-    if PEnd = 0 then
-      PEnd := Length(Str);
-    Station := Copy(Str, P, PDel - P);
-    Temp := Copy(Str, PDel + 1, PEnd - PDel - 1);
-    if (Station <> '') and (Temp <> '') then
-      AddToHashList(Station, Temp);
-    P := PEnd + 1;
-  until (PDel = 0) or (PEnd = 0);
+    Inc(PCur);
+    if DoTemp then
+    begin
+       IsNeg := False;
+       if ABytes[PCur] = 45 then
+       begin
+         Inc(PCur);
+         IsNeg := True;
+       end;
+       Temp := Ord(ABytes[PCur]) - Ord('0');
+       Inc(PCur);
+       if ABytes[PCur] <> 46 then
+       begin
+         Temp := Temp*10 + (Ord(ABytes[PCur]) - Ord('0'));
+         Inc(PCur)
+       end;
+       Inc(PCur);
+       Temp := Temp + (Ord(ABytes[PCur]) - Ord('0'))/10;
+       if IsNeg then
+         Temp := -Temp;
+       Inc(PCur);
+       DoTemp := False;
+    end;
+    if ABytes[PCur] = 59 then
+    begin
+      PDel := PCur;
+      DoHash := False;
+      DoTemp := True;
+      IsNeg := False;
+    end;
+    if DoHash and (ABytes[PCur] <> 10) then
+    begin
+      Hash := Hash xor QWord(ABytes[PCur]); // FNV-1a is XOR then *
+      Hash := Hash * 1099511628211;
+    end;
+    if (ABytes[PCur] = 10) and (PCur < Len) then
+    begin
+      SetLength(Station, PDel - PBeg);
+      Move(ABytes[PBeg], Station[0], PDel - PBeg);
+      AddToHashList(Station, Temp, Hash);
+      Hash := 14695981039346656037;
+      DoHash := True;
+      Temp := 0;
+      DoTemp := False;
+      PBeg := PCur + 1;
+    end;
+  until (PCur >= Len);
 end;
 
 procedure TWSThread.Execute;
 var
-  Buffer: string;
   ReadCnt: LongInt;
-  BufferSize: Int64;
+  Size: Int64;
+  Bytes: TBytes;
+  BytesEx: TBytes;
+  Len, LenEx: LongInt;
 begin
   UpdateThreadList(utAdd);
   try
-    FMS.Write(Pointer(FStr)^, Length(FStr) div SizeOf(Char));
+    FMS.Write(FBytes[0], Length(FBytes));
     FMS.Position := 0;
-    FStr := '';
-    BufferSize := 1048576;
+    FBytes := nil;
+    Size := 1048576;
     repeat
-       if Terminated then
-         Break;
-       Buffer := '';
-       if BufferSize > FMS.Size - FMS.Position then
-         BufferSize := FMS.Size - FMS.Position;
-       SetLength(Buffer, BufferSize div SizeOf(Char));
-       ReadCnt := FMS.Read(Pointer(Buffer)^, BufferSize div SizeOf(Char));
-       if (ReadCnt > 0) then
-       begin
-         if Terminated then Break;
-         Buffer := Buffer + GetLeftOver(FMS);
-         ProcessBuffer(Buffer);
-       end;
+      Bytes := nil;
+      BytesEx := nil;
+      if Size > FMS.Size - FMS.Position then
+        Size := FMS.Size - FMS.Position;
+      SetLength(Bytes, Size);
+      ReadCnt := FMS.Read(Bytes[0], Size);
+      if (ReadCnt > 0) then
+      begin
+        BytesEx := GetNextLineBreak(FMS);
+        LenEx := Length(BytesEx);
+        if LenEx > 0 then
+        begin
+          Len := Length(Bytes);
+          SetLength(Bytes, Len + LenEx);
+          Move(BytesEx[0], Bytes[Len], LenEx);
+        end;
+        AddLineBreak(Bytes);
+        ProcessBytes(Bytes);
+      end;
     until (ReadCnt = 0);
   finally
     UpdateThreadList(utRemove);
+  end;
+end;
+
+procedure TWSThread.AddToHashList(AStation: TBytes; ATemp: Single; AHash: QWord);
+var
+  Index: Integer;
+begin
+  Index := AHash and QWord(HashBuckets - 1);
+  while True do
+  begin
+    if FWSList[Index].FStation = nil then
+    begin
+      SetLength(FWSList[Index].FStation, Length(Astation));
+      Move(Astation[0], FWSList[Index].FStation[0], Length(Astation));
+      FWSList[Index].FData^.FMin := Atemp;
+      FWSList[Index].FData^.FMax := Atemp;
+      FWSList[Index].FData^.FTot := Atemp;
+      FWSList[Index].FData^.FCnt := 1;
+      FWSList[Index].FData^.FHash := AHash;
+      Break;
+    end;
+    if CompareMem(@FWSList[Index].FStation[0], @Astation[0], Length(Astation)) then
+    begin
+      FWSList[Index].FData^.FMin := min(FWSList[Index].FData^.FMin, Atemp);
+      FWSList[Index].FData^.FMax := max(FWSList[Index].FData^.FMax, Atemp);
+      FWSList[Index].FData^.FTot := FWSList[Index].FData^.FTot + Atemp;
+      Inc(FWSList[Index].FData^.FCnt);
+      Break;
+    end;
+    Inc(Index);
+    if Index >= HashBuckets then
+      Index := 0;
+  end;
+end;
+
+procedure TWSThread.UpdateMainHashList;
+var
+  I, Index: Integer;
+begin
+  for I := Low(FWSList) to High(FWSList) do
+  begin
+    if FWSList[I].FStation = nil then
+      Continue;
+    Index := FWSList[I].FData^.FHash and QWord(HashBuckets - 1);
+    while True do
+    begin
+      if FWSManager.FWSListAll[Index].FStation = nil then
+      begin
+        SetLength(FWSManager.FWSListAll[Index].FStation, Length(FWSList[I].FStation));
+        Move(FWSList[I].FStation[0], FWSManager.FWSListAll[Index].FStation[0], Length(FWSList[I].FStation));
+        FWSManager.FWSListAll[Index].FData^.FMin := FWSList[I].FData^.FMin;
+        FWSManager.FWSListAll[Index].FData^.FMax := FWSList[I].FData^.FMax;
+        FWSManager.FWSListAll[Index].FData^.FTot := FWSList[I].FData^.FTot;
+        FWSManager.FWSListAll[Index].FData^.FCnt := FWSList[I].FData^.FCnt;
+        Break;
+      end;
+      if CompareMem(@FWSManager.FWSListAll[Index].FStation[0], @FWSList[I].FStation[0], Length(FWSList[I].FStation)) then
+      begin
+        FWSManager.FWSListAll[Index].FData^.FMin := min(FWSManager.FWSListAll[Index].FData^.FMin, FWSList[I].FData^.FMin);
+        FWSManager.FWSListAll[Index].FData^.FMax := max(FWSManager.FWSListAll[Index].FData^.FMax, FWSList[I].FData^.FMax);
+        FWSManager.FWSListAll[Index].FData^.FTot := FWSManager.FWSListAll[Index].FData^.FTot + FWSList[I].FData^.FTot;
+        FWSManager.FWSListAll[Index].FData^.FCnt := FWSManager.FWSListAll[Index].FData^.FCnt + FWSList[I].FData^.FCnt;
+        Break;
+      end;
+      Inc(Index);
+      if Index >= HashBuckets then
+        Index := 0;
+    end;
   end;
 end;
 
@@ -412,23 +500,27 @@ end;
 procedure TWSThreadsWatcher.CreateFinalList;
 var
   I: Integer;
+  WS: TWS;
   Str: String;
-  WS: PWS;
+  Name: RawByteString;
+  Mean: Single;
   SL: TStringList;
-//  MS: TMemoryStream;
 begin
   SL := TStringList.Create;
   try
     SL.DefaultEncoding := TEncoding.UTF8;
     SL.BeginUpdate;
-    for I := 0 to FWSManager.FHashListAll.Count - 1 do
+    I := High(FWSManager.FWSListALL);
+    for I := Low(FWSManager.FWSListALL) to High(FWSManager.FWSListALL) do
     begin
-      WS := FWSManager.FHashListAll.Items[I];
-      if Trim(WS^.FName) = '' then
+      WS := FWSManager.FWSListALL[I];
+      if WS.FStation = nil then
         Continue;
-      WS^.FTot := Round(WS^.FTot*10)/10;
-      WS^.FMean := WS^.FTot/WS^.FCnt;
-      Str := WS^.FName + '=' + FormatFloat('0.0', WS^.FMin) + '/' + FormatFloat('0.0', WS^.FMean) + '/' + FormatFloat('0.0', WS^.FMax) + ',';
+      SetString(Name, Pointer(@WS.FStation[0]), Length(WS.FStation));
+      SetCodePage(Name, CP_UTF8, True);
+      WS.FData^.FTot := Round(WS.FData^.FTot*10)/10;
+      Mean := WS.FData^.FTot/WS.FData^.FCnt;
+      Str := Name + '=' + FormatFloat('0.0', WS.FData^.FMin) + '/' + FormatFloat('0.0', Mean) + '/' + FormatFloat('0.0', WS.FData^.FMax) + ',';
       SL.Add(Str);
     end;
     SL.EndUpdate;
@@ -437,66 +529,61 @@ begin
   finally
     SL.Free;
   end;
-
   Str := Trim(Str);
   Delete(Str, Length(Str), 1);
   Str := '{' + Str + '}';
   Str := StringReplace(Str, sLineBreak, ' ', [rfReplaceAll]);
   Writeln(Str);
-
-  //Str := Trim(Str);
-  //Delete(Str, Length(Str), 1);
-  //Str := '{' + Str + '}';
-  //MS := TMemoryStream.Create;
-  //try
-  //  MS.Write(Pointer(Str)^, Length(Str) div SizeOf(Char));
-  //  MS.Position := 0;
-  //  MS.SaveToFile(FWSManager.FDstFile);
-  //finally
-  //  MS.Free;
-  //end;
 end;
 
 procedure TWSThreadsWatcher.Execute;
 var
   ThreadCnt: Integer;
-  WSThread: TWSThread;
-  Buffer: String;
-  BufferSize: Int64;
+  Bytes: TBytes;
+  BytesEx: TBytes;
+  Size: Int64;
   FS: TFileStream;
   ReadCnt: LongInt;
+  Len, LenEx: LongInt;
+  WSThread: TWSThread;
 begin
   FStarted := True;
   if (not Terminated) and (FWSManager.FOnStart <> nil) then
     FWSManager.FOnStart(Self);
   FS := TFileStream.Create(FWSManager.FSrcFile, fmOpenRead or fmShareDenyNone);
   try
-    BufferSize := Round(FS.Size/(FWSManager.FThreadCnt + 1));
+    Size := Round(FS.Size/(FWSManager.FThreadCnt + 1));
     FS.Position := 0;
     repeat
-      Buffer := '';
-      if BufferSize > FS.Size - FS.Position then
-        BufferSize := FS.Size - FS.Position;
-      SetLength(Buffer, BufferSize div SizeOf(Char));
-      ReadCnt := FS.Read(Pointer(Buffer)^, BufferSize div SizeOf(Char));
+      Bytes := nil;
+      BytesEx := nil;
+      if Size > FS.Size - FS.Position then
+        Size := FS.Size - FS.Position;
+      SetLength(Bytes, Size);
+      ReadCnt := FS.Read(Bytes[0], Size);
       if (ReadCnt > 0) then
       begin
-        if Terminated then Break;
-        Buffer := Buffer + GetLeftOver(FS);
-        WSThread := TWSThread.Create(Buffer, FWSManager);
+        BytesEx := GetNextLineBreak(FS);
+        LenEx := Length(BytesEx);
+        if LenEx > 0 then
+        begin
+          Len := Length(Bytes);
+          SetLength(Bytes, Len + LenEx);
+          Move(BytesEx[0], Bytes[Len], LenEx);
+        end;
+        AddLineBreak(Bytes);
+        WSThread := TWSThread.Create(Bytes, FWSManager);
         WSThread.Start;
       end;
-    until ReadCnt = 0;
+    until (ReadCnt = 0);
   finally
     FS.Free
   end;
-
   ThreadCnt := -1;
   repeat
     ThreadCnt := GetThreadCnt;
-    Sleep(100);
+    Wait(100);
   until ThreadCnt = 0;
-
   if (not Terminated) then
     CreateFinalList;
   if (not Terminated) and (FWSManager.FOnFinish <> nil) then
@@ -504,28 +591,32 @@ begin
 end;
 
 { TWSManager }
-constructor TWSManager.Create(ASrcFile: String; AThreadCnt: Integer);
+constructor TWSManager.Create(ASrcFile:String; AThreadCnt: Integer);
+var
+  I: Integer;
 begin
   FSrcFile := ASrcFile;
   FThreadCnt := AThreadCnt;
   FTerminated := False;
+  for I := Low(FWSListAll) to High(FWSListAll) do
+  begin
+    FWSListAll[I].FStation := nil;
+    New(FWSListAll[I].FData);
+    FWSListAll[I].FData^.FMin := 0;
+    FWSListAll[I].FData^.FMax := 0;
+    FWSListAll[I].FData^.FTot := 0;
+    FWSListAll[I].FData^.FCnt := 0;
+  end;
   FThreadList := TLockList.Create;
-  FHashListAll := TFPHashList.Create;
   FWSThreadsWatcher := TWSThreadsWatcher.Create(Self);
 end;
 
 destructor TWSManager.Destroy;
 var
   I: Integer;
-  WS: PWS;
 begin
-  for I := 0 to FHashListAll.Count - 1 do
-  begin
-    WS := FHashListAll.Items[I];
-    Dispose(WS);
-  end;
-  FHashListAll.Clear;
-  FHashListAll.Free;
+  for I := Low(FWSListAll) to High(FWSListAll) do
+    Dispose(FWSListAll[I].FData);
   FThreadList.Free;
   inherited Destroy;
 end;
